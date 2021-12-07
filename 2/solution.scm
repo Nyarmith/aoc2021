85313f7b69075617aff5f61640fc372a7e73a3a0
nyarmith <powah.serge@gmail.com>
2021-12-07 14:01:05 -0500

    handle second half of problem in 2

--- 2/solution.scm
+++ 2/solution.scm
@@ -9,17 +9,20 @@
        '()
     )))
 
-(define (get-submarine-pos cur l)
+(define (get-submarine-pos cur l) ; in cur - position 0 is forward, 1 is aim, 2 is depth
   (if (null? l)
     cur
     (begin
       (let ((x (vector-ref cur 0))
           (y (vector-ref cur 1))
+          (z (vector-ref cur 2))
           (cmd (car l)))
       (let ((dir (car cmd))
            (shift (string->number (cadr cmd))))
         (if (string=? dir "forward")
-          (vector-set! cur 0 (+ x shift))
+          (begin
+            (vector-set! cur 0 (+ x shift))
+            (vector-set! cur 2 (+ z (* y shift))))
           (if (string=? dir "up")
             (vector-set! cur 1 (- y shift))
             (vector-set! cur 1 (+ y shift))
@@ -27,5 +30,5 @@
         (get-submarine-pos cur (cdr l)))))
 
 (define (main args)
-  (let ((final-pos (get-submarine-pos (vector 0 0) (input-list))))
-    (display (* (vector-ref final-pos 0) (vector-ref final-pos 1)))))
+  (let ((final-pos (get-submarine-pos (vector 0 0 0) (input-list))))
+    (display (* (vector-ref final-pos 0) (vector-ref final-pos 2)))))
